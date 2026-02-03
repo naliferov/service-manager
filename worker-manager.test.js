b9817eb880df76a243b0c8289da8788e59e05c8f
Nick Aliferov <x8core@gmail.com>
2026-02-03 20:35:10 +0200

    start writing tests

---
+++ worker-manager.test.js
@@ -0,0 +1,5 @@
+import { createWorkerManager } from './worker-manager.js'
+import { rm, mkdtemp } from 'node:fs/promises'
+
+//const workersPath = './test/fixtures/workers'
+//console.log(createWorkerManager)
